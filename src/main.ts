ffbed93df666b05e2bb13be8b0b4f57a6ea6ccfe
thembones79 <michal.szumnarski@gmail.con>
2023-12-02 01:37:09 +0100

    enemy

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,18 @@ const canvas = document.getElementById('canvas1') as HTMLCanvasElement
 const ctx = canvas.getContext('2d')
 
 const CANVAS_HEIGHT = 1000
+const CANVAS_WIDTH = 500
+
+canvas.width = CANVAS_WIDTH
+canvas.height = CANVAS_HEIGHT
+
+const enemy1 = {
+    x: 0,
+    y: 0,
+    width: 200,
+    height: 200,
+}
+
+const animate = () => {
+    ctx?.fillRect(0, 0, 200, 200)
+}
