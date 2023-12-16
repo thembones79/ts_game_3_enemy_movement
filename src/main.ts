7daa3659ef276ed70afa09f3cbf7064465d38744
thembones79 <michal.szumnarski@gmail.con>
2023-12-16 18:42:02 +0100

    added another movement pattern

--- src/main.ts
+++ src/main.ts
@@ -14,13 +14,10 @@ const CANVAS_WIDTH = 500
 canvas.width = CANVAS_WIDTH
 canvas.height = CANVAS_HEIGHT
 
-const numberOfEnemies = 100
+const numberOfEnemies = 300
 const enemies: Enemy[] = []
 
-const gameFrame = 0
-
-const enemyImage = new Image()
-enemyImage.src = 'assets/enemy1.png'
+let gameFrame = 0
 
 class Enemy {
     x: number
@@ -30,27 +27,41 @@ class Enemy {
     spriteWidth: number
     height: number
     speed: number
+    flapSpeed: number
     frame: number
+    angle: number
+    curve: number
+    angleSpeed: number
+    image: HTMLImageElement
+
     constructor() {
-        this.x = Math.random() * canvas.width
-        this.y = Math.random() * canvas.height
-        this.speed = Math.random() * 4 - 2
-        this.spriteWidth = 293
-        this.spriteHeight = 155
+        this.image = new Image()
+        this.image.src = 'assets/enemy2.png'
+        this.speed = Math.random() * 4 + 1
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1)
+        this.spriteWidth = 266
+        this.spriteHeight = 188
         this.width = this.spriteWidth * 0.4
         this.height = this.spriteHeight * 0.4
+        this.x = Math.random() * (canvas.width - this.width)
+        this.y = Math.random() * (canvas.height - this.height)
         this.frame = 0
+        this.angle = Math.random() * 2
+        this.angleSpeed = Math.random() * 0.2
+        this.curve = Math.random() * 7
     }
     update() {
-        this.x += this.speed
-        this.y += this.speed
-        if (gameFrame % 2 === 0)
+        this.x -= this.speed
+        this.y += this.curve * Math.sin(this.angle)
+        this.angle += this.angleSpeed
+        if (this.x + this.width < 0) this.x = canvas.width
+        if (gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? (this.frame = 0) : this.frame++
+        }
     }
     draw() {
-        ctx?.strokeRect(this.x, this.y, this.width, this.height)
         ctx?.drawImage(
-            enemyImage,
+            this.image,
             this.frame * this.spriteWidth,
             0,
             this.spriteWidth,
@@ -74,7 +85,64 @@ for (let i = 0; i < numberOfEnemies; i++) {
 const animate = () => {
     ctx?.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     enemies.forEach((enemy) => enemy.animate())
+    gameFrame++
     requestAnimationFrame(animate)
 }
 
 animate()
+
+// class Enemy1 {
+//     x: number
+//     y: number
+//     width: number
+//     spriteHeight: number
+//     spriteWidth: number
+//     height: number
+//     // speed: number
+//     flapSpeed: number
+//     frame: number
+//     image: HTMLImageElement
+//
+//     constructor() {
+//         this.image = new Image()
+//         this.image.src = 'assets/enemy1.png'
+//         // this.speed = Math.random() * 4 - 2
+//         this.flapSpeed = Math.floor(Math.random() * 3 + 1)
+//         this.spriteWidth = 293
+//         this.spriteHeight = 155
+//         this.width = this.spriteWidth * 0.4
+//         this.height = this.spriteHeight * 0.4
+//         this.x = Math.random() * (canvas.width - this.width)
+//         this.y = Math.random() * (canvas.height - this.height)
+//         this.frame = 0
+//     }
+//     update() {
+//         this.x += Math.random() * 6 - 3
+//         this.y += Math.random() * 9 - 5
+//         if (gameFrame % this.flapSpeed === 0) {
+//             this.frame > 4 ? (this.frame = 0) : this.frame++
+//         }
+//     }
+//     draw() {
+//         ctx?.drawImage(
+//             this.image,
+//             this.frame * this.spriteWidth,
+//             0,
+//             this.spriteWidth,
+//             this.spriteHeight,
+//             this.x,
+//             this.y,
+//             this.width,
+//             this.height
+//         )
+//     }
+//     animate() {
+//         this.update()
+//         this.draw()
+//     }
+// }
+//
+// for (let i = 0; i < numberOfEnemies; i++) {
+//     enemies.push(new Enemy1())
+// }
+//
